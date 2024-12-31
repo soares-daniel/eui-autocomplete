01fb68b653481751ed2e6ca4fd4728a5596eca29
GitHub Action <action@github.com>
2024-12-31 18:23:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/soaresdaniel/euiautocomplete/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/soaresdaniel/euiautocomplete/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.soaresdaniel.euiautocomplete.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
